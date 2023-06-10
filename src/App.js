2343c35936efe7cd6986f72ff6774731f97b9e18
wichan7 <wichan7@naver.com>
2023-06-11 01:03:43 +0900

    modi: id/pw logging

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const userIdRef = useRef(null);
+  const passwordRef = useRef(null);
 
   return (
     <div className="App">
@@ -11,13 +13,20 @@ function App() {
       <div 
         className="login-form">
         <span className="title">로그인</span>
-        <input id="userId" type="text" placeholder="ID"/>
-        <input id="password" type="password" placeholder="password"/>
-        <div id="submit" type="submit" className="submit">제출</div>
+        <input ref={userIdRef} type="text" placeholder="ID"/>
+        <input ref={passwordRef} type="password" placeholder="password"/>
+        <div type="submit" className="submit" onClick={ goLogin }>제출</div>
       </div>
-
     </div>
   );
+
+  function goLogin(event) {
+    let userId = userIdRef.current.value;
+    let password = passwordRef.current.value;
+  
+    console.log(userId, password);
+  }
+  
 }
 
 function Toolbar() {
@@ -29,10 +38,6 @@ function Toolbar() {
       </div>
     </>
   )
-}
-
-function login() {
-  
 }
 
 export default App;
