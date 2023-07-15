83b24a39ff61b37f736d04a4d7b8a6530b6ad9b7
wichan7 <wichan7@naver.com>
2023-07-16 02:49:53 +0900

    Feat: Dialog의 draggable한 노드 추가

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,42 @@
 import React, { useRef, useState, useContext, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import './dashboard.css';
+import Draggable from 'react-draggable';
 
 function Dashboard() {
+  let init = {
+    dialogId: "1",
+    nodes: [
+      {
+        nodeId: "welcome",
+        type: "welcome",
+        label: "welcome"
+      }, {
+        nodeId: "1",
+        type: "output",
+        label: "o1"
+      }, {
+        nodeId: "2",
+        type: "set",
+        label: "s2"
+      }, {
+        nodeId: "3",
+        type: "output",
+        label: "o3"
+      }, {
+        nodeId: "anything-else",
+        type: "anything-else",
+        label: "anything-else"
+      }
+    ]
+  };
+  const [dialog, setDialog] = useState(init);
+
+  function addNode() {
+    let d = { ...dialog };
+    d.nodes.push({ nodeId: "d", type: "set" });
+    setDialog(d);
+  }
 
   return (
     <>
@@ -13,18 +46,25 @@ function Dashboard() {
         </div>
         <div className="dialog-editor">
           <ul className="option-nav">
-            <li>
-              option1
-            </li>
-            <li>
-              option2
-            </li>
-            <li>
-              option3
-            </li>
+            <li onClick={() => addNode()}>더하기</li>
+            <li>option2</li>
+            <li>option3</li>
           </ul>
           <div className="dialog">
-            <div className="panel">panel</div>
+            <div className="panel">
+              {
+                dialog.nodes.map(e => (
+                  <Draggable>
+                    <span id="node" className={e.type}>
+                      <span>
+                        <span>{e.type}</span>
+                        <span>{e.label}</span>
+                      </span>
+                    </span>
+                  </Draggable>
+                ))
+              }
+            </div>
             <div className="list">list</div>
           </div>
         </div>
