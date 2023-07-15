2bf26ad90ff199e110bad7e8c8dbb082d6f15201
wichan7 <wichan7@naver.com>
2023-07-15 20:43:34 +0900

    Feat: Dashboard 구조 생성

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -7,7 +7,28 @@ function Dashboard() {
 
   return (
     <>
-      dd
+      <div className="dashboard-root">
+        <div className="title">
+          <span>챗봇명: TEST</span>
+        </div>
+        <div className="dialog-editor">
+          <ul className="option-nav">
+            <li>
+              option1
+            </li>
+            <li>
+              option2
+            </li>
+            <li>
+              option3
+            </li>
+          </ul>
+          <div className="dialog">
+            <div className="panel">panel</div>
+            <div className="list">list</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
